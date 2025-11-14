36cfa8575f4138ffe3aa413fd75cdc56ec78bdf9
Miguel-Brooks <90522436+Miguel-Brooks@users.noreply.github.com>
2025-11-14 11:23:08 -0600

    Actualización de la base de datos mk2
    Primera conexión con el frontend

--- verifintek/gestor/urls.py
+++ verifintek/gestor/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [
+    path('', views.panel_general, name='panel_general'),
+    path('subempresas/', views.subempresas_view, name='subempresas'),
+    path('captura/', views.captura_view, name='captura'),
+    path('flujo/', views.flujo_view, name='flujo'),
+    path('balance/', views.balance_view, name='balance'),
+]
+
--- verifintek/gestor/views.py
+++ verifintek/gestor/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import SubEmpresa, BalanceHistorico, Cuenta, Poliza
 
-# Create your views here.
+
+def panel_general(request):
+    return render(request, 'gestor/index.html')
+
+def subempresas_view(request):
+    subempresas = SubEmpresa.objects.all()
+    context = {'subempresas': subempresas}
+    return render(request, 'gestor/subempresas.html', context)
+
+def captura_view(request):
+    return render(request, 'gestor/captura.html')
+
+def flujo_view(request):
+    return render(request, 'gestor/flujo.html')
+
+def balance_view(request):
+    balance = BalanceHistorico.objects.all()
+    context = {'balance': balance}
+    return render(request, 'gestor/balance.html', context)
